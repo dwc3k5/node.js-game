5924eda9ae45999468944c12312bb3de47f2d911
dwc3k5 <dwc1608@gmail.com>
2017-10-29 15:15:35 -0500

    making files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'ISfn1R7QINpjk4loqFOznRtBP',
+  consumer_secret: 'IakMqPU5K0mj6HzGPZ5G3RmaBVmIrZMZqmjhNd7vTMHL2CEei3',
+  access_token_key: '924727444329943043-ZQJaMiVtuw4C0PjCUUVr3DMr9dN6gXO',
+  access_token_secret: 'LxkYcGX5xA6MiWo4kn7efEfVqxHpEHdEz81LR9uCqUS09',
+};
+
+module.exports = twitterKeys;
