ef6cdb52b3090eafdd1c5a0c19dd7175b75b1f35
dwc3k5 <dwc1608@gmail.com>
2017-10-30 21:17:15 -0500

    twitter working

--- keys.js
+++ keys.js
@@ -4,7 +4,14 @@ var twitterKeys = {
   consumer_key: 'ISfn1R7QINpjk4loqFOznRtBP',
   consumer_secret: 'IakMqPU5K0mj6HzGPZ5G3RmaBVmIrZMZqmjhNd7vTMHL2CEei3',
   access_token_key: '924727444329943043-ZQJaMiVtuw4C0PjCUUVr3DMr9dN6gXO',
-  access_token_secret: 'LxkYcGX5xA6MiWo4kn7efEfVqxHpEHdEz81LR9uCqUS09',
+  access_token_secret: 'LxkYcGX5xA6MiWo4kn7efEfVqxHpEHdEz81LR9uCqUS09'
 };
 
-module.exports = twitterKeys;
+var spotifyKeys ={
+client_ID: '4521125526254f97b76aaec31956f74c',
+client_secret:'396a2c8ec0fd454d8ec73d7b3b054745'
+};
+module.exports = {
+  twitterKeys: twitterKeys,
+  spotifyKeys: spotifyKeys
+};
--- liri.js
+++ liri.js
@@ -1,5 +1,17 @@
-var tKeys = require("./keys.js");
+//getting keys
+
+//access to api keys
+var keys = require("./keys.js");
+
+
+//access to twitter npm
 var twitter = require('twitter');
+
+//access to Spotify npm
+var spotify = require('node-spotify-api');
+// var client = new twitter([ keys.twitterKeys
+// ]);
+//
 var arg = process.argv[2];
 
 //case statement taking in user input and changing the var cmd
@@ -7,17 +19,61 @@ let cmd = '';
 switch(arg){
   case 'my-tweets':
     cmd = 'twitter';
+    myTweets();
     break;
   case 'spotify-this-song':
     cmd = 'spotify';
+    spotifyThisSong();
     break;
   case 'movie-this':
     cmd = 'OMBD';
+    OMDB();
     break;
   case 'do-what-it-says':
     cmd = '???';
+    doIt();
     break;
 }
+
+
+
+//twitter function
+function myTweets(){
+  console.log("tweets");
+  var client = new twitter(keys.twitterKeys);
+  console.log("this = "+client);
+  var params ={
+    screen_name: "heraldOfMachine",
+    count:1
+  };
+  client.get('statuses/user_timeline', params, function(error, tweets, response){
+    console.log("is there an error?");
+    if(!error){
+      console.log("no");
+
+    for (var i=0; i<1; i++){
+      console.log("tweets = "+tweets[i]);
+
+      var returnedData = ("Number: "+(i+1)+"\n"+tweets[i].created_at+"\n" + tweets[i].text + "\n");
+      console.log(returnedData);
+      console.log("=====================");
+    }
+  }else{
+    console.log("yes");
+    console.log(error);
+  }
+  });
+}
+
+
+//spotify function
+function spotifyThisSong(){
+  console.log("spotify");
+  var spotify = new Spotify(keys.spotifyKeys);
+  console.log(spotify);
+
+}
+
 // console.log(cmd);
 
 
